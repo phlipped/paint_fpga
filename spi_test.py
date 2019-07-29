0efc67dbd4387065dd336f8c810d519cdf15fe65
Phil Wright <philwright@google.com>
2019-07-29 11:41:27 +1000

    fix up spi_test.py, and add .gitignore

--- spi_test.py
+++ spi_test.py
@@ -3,6 +3,13 @@ from migen import *
 from spi import Spi
 
 if __name__  == "__main__":
-  dut = Spi()
-  run_simulation(dut, _test(dut.spi_clk, dut.ss, dut.miso, dut.mosi), vcd_name="spi_sim.vcd")
+  ss = Signal()
+  spi_clk = Signal()
+  miso = Signal()
+  mosi = Signal()
+  data_in = Signal(8)
+  data_in_ready = Signal()
+
+  dut = Spi(spi_clk, ss, miso, mosi, data_in, data_in_ready)
+  # run_simulation(dut, test(dut.spi_clk, dut.ss, dut.miso, dut.mosi), vcd_name="spi_sim.vcd")
 
