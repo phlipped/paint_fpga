e793f12682fb26c057fa2c118203720bfdac0070
Phil Wright <phlipped@gmail.com>
2019-08-08 11:47:11 +1000

    converted spi to use spi_clk directly as the clock. Completely untested, not sure what this means for getting values in and out of the module

--- spi.py
+++ spi.py
@@ -1,11 +1,9 @@
 from nmigen import *
 from nmigen.cli import main
-from edge_detect import EdgeDetect
 
 class Spi(Module):
     '''Basic Spi Module.'''
     def __init__(self, width=8):
-        self.spi_clk = Signal()
         self.ss = Signal()  # FIXME make reset inverted?
         self.mosi = mosi = Signal()
         self.miso = miso = Signal()
@@ -14,31 +12,25 @@ class Spi(Module):
 
     def elaborate(self, platform):
         m = Module()
-
-        m.submodules.spi_edge = spi_edge = EdgeDetect(rising = True)
-        spi_edge.sig = self.spi_clk
-
-        spi_clk_rising = spi_edge.rising
+        m.domains.spi = ClockDomain()
         bit_count = Signal(max=len(self.in_reg))
 
         with m.If(~self.ss):
-            with m.If(spi_clk_rising):
-                m.d.sync += bit_count.eq(bit_count + 1),  # Auto wrap around, right?
-                with m.If(bit_count == len(self.in_reg) - 1):  # FIXME better to check if bit_count is at it's own max value? just not sure how to do that
-                    # Reader of in_reg is expected to clear data_in_ready after
-                    # they've read the value from in_reg.
-                    m.d.sync += self.in_reg_ready.eq(1)
-                m.d.sync += self.in_reg.eq(Cat(self.mosi, self.in_reg[:-1]))
+            m.d.spi += bit_count.eq(bit_count + 1)  # Auto wrap around, right?
+            with m.If(bit_count == (2**(len(bit_count)) - 1)):  # FIXME better way to do this?
+                # Reader of in_reg is expected to clear in_reg_ready after
+                # they've read the value from in_reg.
+                m.d.spi += self.in_reg_ready.eq(1)
+            m.d.spi += self.in_reg.eq(Cat(self.mosi, self.in_reg[:-1]))
         with m.Else():
             # ss is high, so we should just reset everything. FIXME try to
             # re-use built-in reset to achieve this? which would automatically
-            # propogate to our submodules (right?) FIXME reset our edge_detect
-            # module
-            m.d.sync += self.in_reg.eq(0)
-            m.d.sync += self.in_reg_ready.eq(0)
-            m.d.sync += bit_count.eq(0)
+            # propogate to our submodules (right?)
+            m.d.spi += self.in_reg.eq(0)
+            m.d.spi += self.in_reg_ready.eq(0)
+            m.d.spi += bit_count.eq(0)
         return m
 
 if __name__ == '__main__':
     spi = Spi(8)
-    main(spi, ports=[spi.spi_clk, spi.ss, spi.mosi, spi.miso, spi.in_reg, spi.in_reg_ready])
+    main(spi, ports=[spi.ss, spi.mosi, spi.miso, spi.in_reg, spi.in_reg_ready])
