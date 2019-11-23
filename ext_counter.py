cc51f6f4901f30b5bc0dca1f66625e4bff00b7b9
Phil Wright <phlipped@gmail.com>
2019-11-23 22:40:27 +1100

    Use a stdlib FFSynchronizer rather than my own flip flop thingo

--- ext_counter.py
+++ ext_counter.py
@@ -1,20 +1,31 @@
 '''Implements a counter driven by an external clock'''
 
 from nmigen import *
+from nmigen.lib.cdc import FFSynchronizer
 from nmigen.cli import main
 
 class ExtCounter(Elaboratable):
   def __init__(self):
-    self.count = Signal(22)
+    self.o = Signal()
 
   def elaborate(self, platform):
-    ext_clock = platform.request('ext_clock')
-
     m = Module()
-    m.domains.ext = ClockDomain('ext')
 
-    m.d.ext += self.count.eq(self.count + 1)
+    # Declare a new clock domain
+    m.domains.ext = ClockDomain('ext')
+    # Get 'ext_clock' resource from the platform
+    ext_clock = platform.request('ext_clock')
+    # Bind the ext_clock to the clock signal of our new clock domain
     m.d.comb += ClockSignal('ext').eq(ext_clock)
+
+    # Increment counter based on external clock signal
+    counter = Signal(22)
+    m.d.ext += counter.eq(counter + 1)
+
+    # Bind the highest bit of our counter to the input of FFSynchronizer
+    # and bind the output of FFSynchronizer to self.o
+    m.submodules.ffsync = FFSynchronizer(counter[-1], self.o)
+
     return m
 
 if __name__ == '__main__':
--- top.py
+++ top.py
@@ -19,12 +19,9 @@ class Top(Elaboratable):
     led = platform.request('led')
     main_clock_out = platform.request('main_clock_out')
     counter = Signal(22)
-    flops_from_ext = Signal(3)
 
-    m.d.sync += [counter.eq(counter + 1),
-                 flops_from_ext[0].eq(ext.count),
-                 flops_from_ext[1:].eq(flops_from_ext[:-1]),
-                 led.eq(flops_from_ext[-1]),]
+    m.d.sync += counter.eq(counter + 1)
+    m.d.comb += led.eq(ext.o)
 
     # Bind 5 bits of main_clock_out to the bottom 5 bits of self.counter
     m.d.comb += [main_clock_out.eq(counter[:4])]
