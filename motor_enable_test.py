e1822e082dd10bb1cdcbf30c8dbe2b03fdf2ccd7
Phil Wright <phlipped@gmail.com>
2019-12-02 21:16:19 +1100

    update unittest to test both directions.

--- motor_enable_test.py
+++ motor_enable_test.py
@@ -15,24 +15,33 @@ class ShiftRegisterTest(FHDLTestCase):
             gtkw_file=gtkw_file) as sim:
             sim.add_clock(1e-6)
             def process():
-                yield self.dut.direction.eq(1)
-                yield self.dut.limit_bottom.eq(0)
-                yield self.dut.enable_i.eq(1)
-                yield
-                yield self.dut.enable_i.eq(0)
-                yield
-                yield self.dut.enable_i.eq(1)
-                yield
-                yield self.dut.enable_i.eq(0)
-                yield
-                yield self.dut.limit_bottom.eq(1)
-                yield self.dut.enable_i.eq(1)
-                yield
-                yield self.dut.enable_i.eq(0)
-                yield
-                yield self.dut.enable_i.eq(1)
-                yield
-                yield self.dut.enable_i.eq(0)
-                yield
+                for dir, limit in [(1, self.dut.limit_bottom), (0, self.dut.limit_top)]:
+                    yield self.dut.direction.eq(dir)     # down
+                    yield limit.eq(0)
+                    yield self.dut.enable_i.eq(1)
+                    yield
+                    assert (yield self.dut.enable_o == 1)
+                    yield self.dut.enable_i.eq(0)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
+                    yield self.dut.enable_i.eq(1)
+                    yield
+                    assert (yield self.dut.enable_o == 1)
+                    yield self.dut.enable_i.eq(0)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
+                    yield limit.eq(1)
+                    yield self.dut.enable_i.eq(1)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
+                    yield self.dut.enable_i.eq(0)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
+                    yield self.dut.enable_i.eq(1)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
+                    yield self.dut.enable_i.eq(0)
+                    yield
+                    assert (yield self.dut.enable_o == 0)
             sim.add_sync_process(process())
             sim.run()
