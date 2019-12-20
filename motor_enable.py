b18b05072d5d8af8908446e6427c332e2658ad51
Phil Wright <phlipped@gmail.com>
2019-12-20 23:27:38 +1100

    .

--- motor_enable.py
+++ motor_enable.py
@@ -5,19 +5,16 @@ class MotorEnable(Elaboratable):
         self.limit_top = Signal() # 1 means it's been hit, 0 means not hit
         self.limit_bottom = Signal() # 1 means it's been hit, 0 means ok
         self.direction = Signal() # 1 means 'down', 0 means 'up'
-        self.enable_i = Signal()
-        self.enable_o = Signal()
-        self.is_test = is_test
+        self.enable_i = Signal() # incoming enable signal from paint controller
+        self.enable_o = Signal() # outgoing enable signal to motors
+        self.is_test = is_test # create a dummy signal so that we have a clock
         if is_test:
           self.dummy = Signal()
 
     def elaborate(self, platform):
         m = Module()
         if self.is_test:
-          with m.If(self.dummy == 1):
-              m.d.sync += self.dummy.eq(0)
-          with m.Else():
-              m.d.sync += self.dummy.eq(1)
+            m.d.sync += self.dummy.eq(~self.dummy)
 
         with m.If(self.direction == 1):  # direction is down
             with m.If(self.limit_bottom == 1):
